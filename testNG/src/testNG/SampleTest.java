5865f8576d289cca179170f8c8d896833b46f7c5
Hariraj07 <hariharan7492@gmail.com>
2022-05-27 09:07:14 +0530

    Code format

---
+++ testNG/src/testNG/SampleTest.java
@@ -0,0 +1,21 @@
+package testNG;
+
+public class SampleTest {
+	WebDriver driver;
+	
+	public <WebDriver> void launchbrowser() {
+		System.setProperty("WebDriver.Chrome.Driver", "C:\\Users\\Admin\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+	}
+	
+	public void google(){
+		driver.
+		
+	}
+public void bing() {
+	
+}
+public void firefox() {
+	
+}
+}
